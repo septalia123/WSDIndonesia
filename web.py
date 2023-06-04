260832752c6e98654265857cac99360969bc2f34
septalia123 <72060483+septalia123@users.noreply.github.com>
2023-06-04 15:37:11 +0700

    Update web.py

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from kbbi import AutentikasiKBBI
 from kbbi import KBBI
 import streamlit as st
 import nltk
+nltk.download('stopwords')
 import re
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
