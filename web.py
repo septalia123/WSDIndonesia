8b6f67ae1f058d6190d5d477692e22a7b6a26e8b
septalia123 <72060483+septalia123@users.noreply.github.com>
2023-06-18 23:03:08 +0700

    Update web.py

--- web.py
+++ web.py
@@ -3,8 +3,6 @@ from kbbi import AutentikasiKBBI
 from kbbi import KBBI
 import streamlit as st
 import nltk
-nltk.download('stopwords')
-import re
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from nltk.tokenize import sent_tokenize
@@ -24,12 +22,14 @@ kalimat = st.text_input("Inputkan Kalimat: ")
 proses = st.button("Proses Kalimat")
 
 # ====================================================================== PREPROCESSING DATA ===============================
+
+
 def preproces(kalimat):
     lower = kalimat.lower()
 
     tokenisasi = word_tokenize(lower)
-        
-    #proses Stopword Removal
+
+    # proses Stopword Removal
     stopWords.remove('asal')
     stopWords.remove('atas')
     stopWords.remove('besar')
@@ -43,31 +43,39 @@ def preproces(kalimat):
     stopWords.remove('panjang')
     stopWords.remove('semata')
     stopWords.remove('waktu')
+    stopWords.remove('tahun')
     stopWordsd = set(stopWords)
     word_tokens_no_stopwords = [w for w in tokenisasi if not w in stopWordsd]
-    
-    #menghapus karakter dan menggantinya dengan spasi/karakter kosong
+
+    # menghapus karakter dan menggantinya dengan spasi/karakter kosong
     special_char = "+=`@_!#$%^&*()<>?/\|}{~:;.[],1234567890‘’'" + '"“”●'
-    cleaning = [''.join(x for x in string if not x in special_char) for string in word_tokens_no_stopwords]
+    cleaning = [''.join(x for x in string if not x in special_char)
+                for string in word_tokens_no_stopwords]
     while '' in cleaning:
         cleaning.remove('')
     return cleaning
 
+
 cleaning = preproces(kalimat)
 
 # ====================================================================== PEMBENTUKAN N-DATA ===============================
+
+
 def ngram(data, n):
     gram = []
     for ng in cleaning:
-        temp=zip(*[cleaning[ng:] for ng in range(0,n)])
-        gram=[' '.join(n) for n in temp]
+        temp = zip(*[cleaning[ng:] for ng in range(0, n)])
+        gram = [' '.join(n) for n in temp]
     return gram
+
 
 unigram = ngram(cleaning, 1)
 bigram = ngram(cleaning, 2)
 trigram = ngram(cleaning, 3)
 
 # ====================================================================== PENCARIAN KATA BERMAKNA ===============================
+
+
 def NewKata(kata):
     new_kata = []
     for nkata in kata:
@@ -83,11 +91,14 @@ def NewKata(kata):
                 del nkata
     return new_kata
 
+
 new_unigram = NewKata(unigram)
 new_bigram = NewKata(bigram)
 new_trigram = NewKata(trigram)
 
 # ====================================================================== MENGAMBIL MAKNA KATA ===============================
+
+
 def makna(kata):
     makna = []
     for nkata in kata:
@@ -95,87 +106,108 @@ def makna(kata):
         cari = KBBI(nkata, auth)
         dmakna = cari.__str__(contoh=True, terkait=False, fitur_pengguna=False)
 
-        #proses memberi index tiap makna dan menghapus makna pertama
+        # proses memberi index tiap makna dan menghapus makna pertama
         stc = sent_tokenize(dmakna)
-        if len(stc)>1:
+        if len(stc) > 1:
             del stc[0]
-        for u in range (len(stc)):
-            #mengubah -- menjadi kata yang dicari
+        for u in range(len(stc)):
+            # mengubah -- menjadi kata yang dicari
             if '--' in stc[u]:
-                stc[u] = stc[u].replace('--', nkata) 
-                    
-            #menghapus tanda baca, angka, dan space pada makna
+                stc[u] = stc[u].replace('--', nkata)
+
+            # menghapus tanda baca, angka, dan space pada makna
             del_angka_makna = re.sub(r"\d+", '', stc[u])
             if "\n" in del_angka_makna:
                 del_angka_makna = del_angka_makna.replace('\n' and '\nn', " ")
                 del_angka_makna = del_angka_makna.replace('.', " ")
             del_spc_makna = del_angka_makna.strip()
             makna_kata.append(del_spc_makna)
-        makna.append(makna_kata)   
+        makna.append(makna_kata)
     return makna
+
 
 makna_unigram = makna(new_unigram)
 makna_bigram = makna(new_bigram)
 makna_trigram = makna(new_trigram)
 
 # ====================================================================== SELEKSI DATA N-GRAM ===============================
+
+
 def skata(dkata):
     seleksi = []
-    for z in new_unigram:
-        seleksi.append(z)
     if len(new_trigram) == 0:
-        for i in new_bigram:
-            pecah = word_tokenize(i)
-            for j in new_unigram:
-                if j == pecah[0]:
-                    idx = seleksi.index(pecah[0])
-                    seleksi[idx] = i
-                    del seleksi[idx+1]
-                    break
-                elif j == pecah[1]:
-                    idx = seleksi.index(pecah[1])
-                    seleksi[idx] = i
-                    del seleksi[idx-1]
-                    break
+        n = 0
+        for db in new_bigram:
+            pecah = word_tokenize(db)
+            while n != len(new_unigram):
+                if new_unigram[n] in db:
+                    if new_unigram[n] == pecah[0]:
+                        seleksi.append(db)
+                        n = new_unigram.index(pecah[0])+2
+                        break
+                    else:
+                        seleksi.append(db)
+                        del seleksi[-2]
+                        n = new_unigram.index(pecah[1])+1
+                        break
+                else:
+                    seleksi.append(new_unigram[n])
+                    n += 1
+
+        if len(seleksi) != (len(new_unigram)-len(new_bigram)):
+            tambah_kata = len(seleksi)+len(new_bigram)
+            for k in range(tambah_kata, len(new_unigram)):
+                seleksi.append(new_unigram[k])
     else:
-        for i in new_trigram:
-            pecah = word_tokenize(i)
-            for j in new_unigram:
-                if j == pecah[0]:
-                    idx = seleksi.index(pecah[0])
-                    seleksi[idx] = i
-                    del seleksi[idx+1]
-                    del seleksi[idx+1]
-                    break
-                elif j == pecah[1]:
-                    idx = seleksi.index(pecah[1])
-                    seleksi[idx] = i
-                    del seleksi[idx-1]
-                    del seleksi[idx]
-                    break
-                elif j == pecah[2]:
-                    idx = seleksi.index(pecah[2])
-                    seleksi[idx] = i
-                    del seleksi[idx-1]
-                    del seleksi[idx-1]
-                    break
-    
-            for k in new_bigram:
-                pecah = word_tokenize(k)
-                for j in new_unigram:
-                    if j == pecah[0]:
+        n = 0
+        for dt in new_trigram:
+            pecah = word_tokenize(dt)
+            while n != len(new_unigram):
+                if new_unigram[n] in dt:
+                    if new_unigram[n] == pecah[0]:
+                        seleksi.append(dt)
+                        n = new_unigram.index(pecah[0])+3
+                        break
+                    elif new_unigram[n] == pecah[1]:
+                        seleksi.append(dt)
+                        n = new_unigram.index(pecah[1])+2
+                        del seleksi[-2]
+                        break
+                    else:
+                        seleksi.append(dt)
+                        n = new_unigram.index(pecah[2])+1
+                        del seleksi[-2]
+                        del seleksi[-2]
+                        break
+                else:
+                    seleksi.append(new_unigram[n])
+                    n += 1
+
+        if len(seleksi) != (len(new_unigram)-len(new_trigram)):
+            tambah_kata = len(seleksi)+len(new_trigram)+(1*len(new_trigram))
+            for k in range(tambah_kata, len(new_unigram)):
+                seleksi.append(new_unigram[k])
+
+        if len(new_bigram) != 0:
+            for db in new_bigram:
+                pecah = word_tokenize(db)
+                for i in seleksi:
+                    if i == pecah[0]:
                         idx = seleksi.index(pecah[0])
-                        seleksi[idx] = k
+                        seleksi[idx] = db
                         del seleksi[idx+1]
-                    elif j == pecah[1]:
+                    elif i == pecah[1]:
                         idx = seleksi.index(pecah[1])
-                        seleksi[idx] = k
+                        seleksi[idx] = db
                         del seleksi[idx-1]
     return seleksi
+
 
 seleksi_kata = skata(new_unigram)
 
 # ====================================================================== SELEKSI MAKNA DATA N-GRAM ===============================
+
+
 def smakna(dmakna):
     seleksi_makna = []
     for j in seleksi_kata:
@@ -193,40 +225,49 @@ def smakna(dmakna):
             seleksi_makna.append(makna)
     return seleksi_makna
 
+
 seleksi_makna = smakna(seleksi_kata)
 
 # ====================================================================== MENGHITUNG SKOR LESK ===============================
+
+
 def skor(seleksi_kata):
     count = 0
-    skor_makna=[]
+    skor_makna = []
     while count != len(seleksi_kata):
         if len(seleksi_makna[count]) == 1:
             skor_makna.append("OK")
         else:
-            skor_makna_kata=[]
+            skor_makna_kata = []
             for i in range(len(seleksi_makna[count])):
                 setmakna = set(word_tokenize(seleksi_makna[count][i]))
                 setkalimat = set(word_tokenize(kalimat.lower()))
-                irisan = setmakna&setkalimat
+                irisan = setmakna & setkalimat
                 skor_makna_kata.append(len(irisan))
             skor_makna.append(skor_makna_kata)
-        count +=1
+        count += 1
     return skor_makna
+
+
 skor_makna = skor(seleksi_kata)
 
 # ====================================================================== MENENTUKAN MAKNA ===============================
+
+
 def SimplifiedLesk(skor_makna):
     count2 = 0
     makna_pilihan = []
-    while count2!= len(skor_makna):
+    while count2 != len(skor_makna):
         if skor_makna[count2] != "OK":
             lmax = max(skor_makna[count2])
             idx = skor_makna[count2].index(lmax)
             makna_pilihan.append(seleksi_makna[count2][idx])
         else:
             makna_pilihan.append(seleksi_makna[count2][0])
-        count2+=1
+        count2 += 1
     return makna_pilihan
+
+
 hasil = SimplifiedLesk(skor_makna)
 
 
@@ -241,23 +282,8 @@ if proses:
             for i in range(len(seleksi_makna[c3])):
                 if seleksi_makna[c3][i] == hasil[c3]:
                     st.success(
-                        f"{hasil[c3]} (Skor Makna : {skor_makna[c3][i]})")
+                        f"{i+1}. {hasil[c3]} (Skor Makna : {skor_makna[c3][i]})")
                 else:
                     st.write(
-                        i+1, seleksi_makna[c3][i], "(Skor Makna : ", skor_makna[c3][i], ")")
+                        i+1,".", seleksi_makna[c3][i], "(Skor Makna : ", skor_makna[c3][i], ")")
         c3 += 1
-    #st.write("Hasil CF = ", cf)
-    #st.write("Hasil Preprocessing = ", cleaning)
-    #st.write("Data Unigram = ", unigram)
-    #st.write("Data Bigram = ", bigram)
-    #st.write("Data Trigram = ", trigram)
-    #st.write("Data Unigram New = ", new_unigram)
-    #st.write("Data Bigram New = ", new_bigram)
-    #st.write("Data Trigram New = ", new_trigram)
-    #st.write("Makna Uni = ", makna_unigram)
-    #st.write("Makna Big = ", makna_bigram)
-    #st.write("Makna Tri = ", makna_trigram)
-    #st.write("Seleksi Kata = ", seleksi)
-    #st.write("Seleksi Makna = ", seleksi_makna)
-    #st.write("Seleksi Makna = ", skor_makna)
-    #st.write("Makna pilihan = ", makna_pilihan)
